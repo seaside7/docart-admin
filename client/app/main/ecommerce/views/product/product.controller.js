ada6e3e28590dddab5b717a128bb21c660919e43
Suhendra Ahmad <syuaibi@gmail.com>
2016-07-31 15:14:13 +0700

    Removed froala

--- client/app/main/ecommerce/views/product/product.controller.js
+++ client/app/main/ecommerce/views/product/product.controller.js
@@ -7,9 +7,6 @@
 
         // Data
         vm.productTitle = 'New Product';
-        vm.froalaOptions = {
-            toolbarButtons : ["bold", "italic", "underline", "|", "align", "formatOL", "formatUL"]
-        }
         vm.taToolbar = [
             ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'pre', 'quote', 'bold', 'italics', 'underline', 'strikeThrough', 'ul', 'ol', 'redo', 'undo', 'clear'],
             ['justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull', 'indent', 'outdent', 'html', 'insertImage', 'insertLink', 'insertVideo', 'wordcount', 'charcount']
